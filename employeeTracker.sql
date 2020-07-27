23b82898e36becc199aa2210e0e90d57c5b83a12
Darren Behan <darrenbehan@hotmail.com>
2020-07-27 20:06:21 +1000

    update to employeeTracker.sql file to create tables with foreign keys for manager, role & department, index.js file changed if/else to switch & added listEmployeesByDepartment function

--- employeeTracker.sql
+++ employeeTracker.sql
@@ -5,37 +5,43 @@ USE employeeTracker_DB;
 
 CREATE TABLE department
 (
-  id INT NOT NULL
+  id INT PRIMARY KEY
   AUTO_INCREMENT,
   name VARCHAR
+  (30) NOT NULL
+);
+
+CREATE TABLE role
+(
+  id INT NOT NULL PRIMARY KEY
+  AUTO_INCREMENT,
+  title VARCHAR
   (30) NOT NULL,
-  PRIMARY KEY
+  salary DECIMAL,
+  department_id INT NOT NULL,
+  CONSTRAINT fk_department FOREIGN KEY
+  (department_id) REFERENCES department
   (id)
 );
 
-  CREATE TABLE role
-  (
-    id INT NOT NULL
-    AUTO_INCREMENT,
-  title VARCHAR
-    (30) NOT NULL,
-  salary DECIMAL
-    (10,2),
-  department_id INT,
-  PRIMARY KEY
-    (id)
+CREATE TABLE employee
+(
+  id INT NOT NULL
+  AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR
+  (30) NOT NULL,
+  last_name VARCHAR
+  (30) NOT NULL,
+  role_id INT NOT NULL,
+  CONSTRAINT fk_role FOREIGN KEY
+  (role_id) REFERENCES role
+  (id),
+  manager_id INT, 
+  CONSTRAINT fk_manager FOREIGN KEY
+  (manager_id) REFERENCES employee
+  (id)
 );
 
-    CREATE TABLE employee
-    (
-      id INT NOT NULL
-      AUTO_INCREMENT,
-  first_name VARCHAR
-      (30) NOT NULL,
-  last_name VARCHAR
-      (30) NOT NULL,
-  role_id INT default 0,
-  manager_id INT,
-  PRIMARY KEY
-      (id)
-);
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const cTable = require('console.table');
+const cTable = require("console.table");
 
 // create the connection information for the sql database
 const connection = mysql.createConnection({
@@ -14,28 +14,26 @@ const connection = mysql.createConnection({
 
   // Your password
   password: "",
-  database: "employeeTracker_DB"
+  database: "employeeTracker_DB",
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   start();
 });
 
 // What would you like to do?
-// View all employees
-  // List employees
-// View all employees by department
-  // Filter by department => OUTER JOIN
 // View all employees by manager
   // Filter by manager => OUTER JOIN
 // Add employee
   // First Name
   // Last Name
   // Role
+    // Required list of roles to choose from
   // Manager
+    // Require list of managers to choose from and an option of "none"
 // Remove employee
   // List employees
 // Update employee role
@@ -56,25 +54,69 @@ function start() {
       name: "databaseOptions",
       type: "list",
       message: "What would you like to do?",
-      choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add employee", "Remove employee", "Update employee role", "Update employee manager", "View all roles", "Exit"]
+      choices: [
+        "View all employees",
+        "View all employees by department",
+        "View all employees by manager",
+        "Add employee",
+        "Remove employee",
+        "Update employee role",
+        "Update employee manager",
+        "View all roles",
+        "Exit",
+      ],
     })
-    .then(function(answer) {
+    .then(function (answer) {
       // based on their answer, call the function relevant to the option selected
-      if (answer.databaseOptions === "View all employees") {
-        listEmployees();
-      } else if (answer.databaseOptions === "Exit") {
-        connection.end();
-      } else {
-        connection.end();
+      switch (answer.databaseOptions) {
+        case "View all employees":
+          listEmployees();
+          break;
+        case "View all employees by department":
+          listEmployeesByDepartment();
+          break;
+        case "Exit":
+          connection.end();
+          break;
       }
     });
 }
 
+// View all employees
+  // id
+  // first_name
+  // last_name
+  // title
+  // department
+  // salary
+  // manager
+
 // Returns a formatted table showing all employees
 function listEmployees() {
+  connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name AS department, role.salary FROM employeeTracker_DB.employee LEFT JOIN employeeTracker_DB.role ON employee.role_id = role.id LEFT JOIN employeeTracker_DB.department on role.department_id = department.id;", function (
+    err,
+    result
+  ) {
+    if (err) {
+      throw err;
+    } else if (result === null) {
+      console.log("No employees have been added yet!")
+    } else {
+      console.table(result);
+    }
+    // re-prompt the user
+    start();
+  });
+}
+
+// View all employees by department
+  // Filter by department => OUTER JOIN
+
+// Returns a formatted table showing all employees by department
+function listEmployeesByDepartment() {
   connection.query(
-    "SELECT * FROM employeeTracker_DB.employee",
-    function(err, result) {
+    "SELECT employee.id, first_name, last_name, role_id, manager_id, department.id, department.department_name FROM ((employeeTracker_DB.employee RIGHT JOIN employeeTracker_DB.role ON role_id = role.id) RIGHT JOIN employeeTracker_DB.department ON department_id = department.id)",
+    function (err, result) {
       if (err) throw err;
       console.table(result);
       // re-prompt the user
